a76fe73f3dd78beeee03b9a5470d66fdd7be2692
Matti Hyyppä <matti.hyyppa@gmail.com>
2024-01-12 19:43:57 +0200

    Update Playwright config and E2E tests

--- e2e/canvas.spec.ts
+++ e2e/canvas.spec.ts
@@ -14,18 +14,23 @@
 
 import { test, expect } from '@playwright/test';
 
-const menuBorderWidth = 1.5;
+import { cmToPixels } from '../src/utils';
+
+const VIEWPORT_WIDTH = 1600;
+const VIEWPORT_HEIGHT = 1000;
 
 // Width of the menu component located on the left-hand side of the canvas.
 // The viewport size has been set in `playwright.config.ts` to a fixed value, such
-// that the menu width is a known value (260 pixels + 1.5 pixels for border-right)
-const menuWidth = 260 + menuBorderWidth;
+// that the menu width is a known value.
+const MENU_WIDTH = 260;
+
 
 test('initially no shape selected', async ({ page }) => {
   await page.goto('/');
   await page.getByRole('tab', { name: 'Edit' }).click();
   await expect(page.getByText('Select a shape to start editing.')).toBeVisible();
 });
+
 
 test('adding a rectangular house works', async ({ page }) => {
   await page.goto('/');
@@ -36,8 +41,8 @@ test('adding a rectangular house works', async ({ page }) => {
   await page.getByText('Rectangle').click();
 
   const initialTopLeftPosition = { x: 100, y: 100 };
-  const xClick = menuWidth + initialTopLeftPosition.x;
-  const yClick = initialTopLeftPosition.y;
+  const xClick = MENU_WIDTH + initialTopLeftPosition.x + 1;
+  const yClick = initialTopLeftPosition.y + 1;
 
   await page.mouse.click(xClick, yClick);
 
@@ -45,4 +50,82 @@ test('adding a rectangular house works', async ({ page }) => {
   // a form for editing the shape should be now visible. The form contains a button
   // for deleting the shape.
   await expect(page.getByRole('button', { name: 'Delete object' })).toBeVisible();
+});
+
+
+test('rectangular house has correct dimensions', async ({ page }) => {
+  // This test case tests clicking the 4 corners of the house
+
+  await page.goto('/');
+
+  // Add a rectangular house on the canvas
+  await page.getByRole('tab', { name: 'Add' }).click();
+  await expect(page.getByRole('heading', { name: 'House shapes' })).toBeVisible();
+  await page.getByText('Rectangle').click();
+
+  const houseTopLeftPosition = {
+    x: 100 + MENU_WIDTH,
+    y: 100,
+  };
+
+  const expectedWidthMeters = 12;
+  const expectedHeightMeters = 10;
+  const expectedWidthPixels = cmToPixels(expectedWidthMeters * 100);
+  const expectedHeightPixels = cmToPixels(expectedHeightMeters * 100);
+
+  let xClick = houseTopLeftPosition.x;
+  let yClick = houseTopLeftPosition.y;
+
+  /********** TOP-LEFT CORNER **********/
+  await page.mouse.click(xClick + 1, yClick + 1);
+
+  // Clicking the shape should open a form for editing the shape.
+  await expect(page.getByRole('button', { name: 'Delete object' })).toBeVisible();
+
+  // Click the bottom-right corner of the canvas (which should be empty) to hide the form
+  await page.mouse.click(VIEWPORT_WIDTH - 1, VIEWPORT_HEIGHT - 1);
+
+  // Click a point that is right outside of the shape
+  await page.mouse.click(xClick - 1, yClick - 1);
+
+  // If no shape on the canvas was clicked, a catalogue of all available shapes should
+  // be visible.
+  await expect(page.getByRole('heading', { name: 'House shapes' })).toBeVisible();
+  /********** TOP-LEFT CORNER **********/
+
+  /********** TOP-RIGHT CORNER **********/
+  xClick = houseTopLeftPosition.x + expectedWidthPixels;
+  yClick = houseTopLeftPosition.y;
+  await page.mouse.click(xClick - 1, yClick + 1);
+  await expect(page.getByRole('button', { name: 'Delete object' })).toBeVisible();
+
+  await page.mouse.click(VIEWPORT_WIDTH - 1, VIEWPORT_HEIGHT - 1);
+
+  await page.mouse.click(xClick + 1, yClick - 1);
+  await expect(page.getByRole('heading', { name: 'House shapes' })).toBeVisible();
+  /********** TOP-RIGHT CORNER **********/
+
+  /********** BOTTOM-LEFT CORNER **********/
+  xClick = houseTopLeftPosition.x;
+  yClick = houseTopLeftPosition.y + expectedHeightPixels;
+  await page.mouse.click(xClick + 1, yClick - 1);
+  await expect(page.getByRole('button', { name: 'Delete object' })).toBeVisible();
+
+  await page.mouse.click(VIEWPORT_WIDTH - 1, VIEWPORT_HEIGHT - 1);
+
+  await page.mouse.click(xClick - 1, yClick + 1);
+  await expect(page.getByRole('heading', { name: 'House shapes' })).toBeVisible();
+  /********** BOTTOM-LEFT CORNER **********/
+
+  /********** BOTTOM-RIGHT CORNER **********/
+  xClick = houseTopLeftPosition.x + expectedWidthPixels;
+  yClick = houseTopLeftPosition.y + expectedHeightPixels;
+  await page.mouse.click(xClick - 1, yClick - 1);
+  await expect(page.getByRole('button', { name: 'Delete object' })).toBeVisible();
+
+  await page.mouse.click(VIEWPORT_WIDTH - 1, VIEWPORT_HEIGHT - 1);
+
+  await page.mouse.click(xClick + 1, yClick + 1);
+  await expect(page.getByRole('heading', { name: 'House shapes' })).toBeVisible();
+  /********** BOTTOM-RIGHT CORNER **********/
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -40,4 +40,10 @@ export default defineConfig({
       },
     },
   ],
+  // Run your local dev server before starting the tests.
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+  },
 });
